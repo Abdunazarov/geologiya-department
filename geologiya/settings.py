2d8d99753d1ecb129e02e6c3a3f6f5ce668abde5
Dior <abdunazarovdior@gmail.com>
2021-11-06 12:29:34 +0500

    ...

--- geologiya/settings.py
+++ geologiya/settings.py
@@ -113,8 +113,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATIC_ROOT = [os.path.join(BASE_DIR), 'static_root']
-# Default primary key field type
-# https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
