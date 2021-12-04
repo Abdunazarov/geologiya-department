db96608dda3239b0b6f06aec02c85982e8a7fe64
shaxboz Sobitov <shaxboz824@icloud.com>
2021-12-04 14:31:34 +0500

    ..

--- geologiya/urls.py
+++ geologiya/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-# from baton.autodiscover import admin
 from django.urls import path, include
-
+from django.conf import settings
+from django.conf.urls.static import static
 
 api = [
     path('', include('mainAPI.urls')),
@@ -15,3 +15,7 @@ urlpatterns = [
     path('baton/', include('baton.urls')),
 
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL,
+                      document_root=settings.STATIC_ROOT)
